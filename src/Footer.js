4fc5f8e69cb0a3f703ad9f02d2e548bdf2278d44
Alexander Peal <aapeal04@gmail.com>
2024-02-17 11:04:02 -0800

    created footer component

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="row">
+            <div className="col-12">
+                <div className="text-center">
+                Footer content goes here
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
